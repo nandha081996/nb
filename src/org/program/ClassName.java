be230268b11dde74f303940c5ed01c6aecf7a1cf
nandha081996 <nandhakumarsubbaiah@gmail.com>
2021-01-22 22:01:18 -0800

    newproject

---
+++ src/org/program/ClassName.java
@@ -0,0 +1,19 @@
+package org.program;
+
+public class ClassName {
+	
+	public void method2(){
+		int x = 2;
+		int y =4;
+		int z=x+y;
+		System.out.println(z);
+	}
+	
+	
+	
+	
+public static void main(String[] args) {
+	ClassName c = new ClassName();
+	c.method2();
+}
+}
